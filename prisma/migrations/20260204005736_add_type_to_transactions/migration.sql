8e0cfea1e871109729187107bb7f5da3ebe40624
Tomás <tomasg92@gmail.com>
2026-02-10 21:49:50 -0400

    chore: removes migrations

--- prisma/migrations/20260204005736_add_type_to_transactions/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "TransactionType" AS ENUM ('EXPENSE', 'INCOME');
-
--- AlterTable
-ALTER TABLE "transactions" ADD COLUMN     "type" "TransactionType" NOT NULL DEFAULT 'EXPENSE';
--- prisma/migrations/20260205135721_extend_currency_field/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "transactions" ALTER COLUMN "currency" TYPE VARCHAR(10);
